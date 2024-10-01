4b8d4f3cc950a675456ea22093ff88f710652f6f
Pedro Souza <pedrops244@hotmail.com>
2024-10-01 17:46:57 -0300

    createUser method

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,10 +1,38 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuário"))
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
 
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuario(db)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id inserido: %d", usuarioID)))
 }
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscando todos os usuarios"))
