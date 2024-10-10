e390c34df62ec5887f80765bb38701e8a4ff0aaa
Pedro Souza <pedrops244@hotmail.com>
2024-10-10 15:13:32 -0300

    created func BuscarPublicacao(id)

--- src/controllers/publicacoes.go
+++ src/controllers/publicacoes.go
@@ -9,6 +9,9 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
@@ -58,7 +61,28 @@ func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
 
 }
 func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoId, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
 
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao, erro := repositorio.BuscarPorId(publicacaoId)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacao)
 }
 func AutalizarPublicacao(w http.ResponseWriter, r *http.Request) {
 
